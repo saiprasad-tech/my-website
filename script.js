4a69fd01b194cf3b2da70426c51ec3591987ee6a
saiprasad-tech <237r5a6709@CMRTC.AC.IN>
2024-12-12 17:55:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
+// Initialize AOS
+AOS.init({
+    duration: 1200,
+});
